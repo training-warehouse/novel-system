b688228399eb491fa78a9657ac647e160a8daa76
liaokongVFX <568250549@qq.com>
2021-11-25 23:19:38 +0800

    完成图书详情页

---
+++ components/tabTop.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="flex align-center border" style="height: 80rpx;justify-content: space-around;">
+		<block v-for="(item,index) in tabArr" :key="index">
+			<view class="flex flex-column align-center justify-between" style="height: 80rpx;"
+				@tap="changeTabIndex(index)">
+				<view class="pt-1 text-light-black animated faster" :class="tabIndex === index ? 'font-weight-bold pluse':''">{{item}}</view>
+				<view class="rounded animated zoomIn faster"
+					style="width: 80rpx;height: 8rpx;background-color: #fb5f39;" v-if="tabIndex === index"></view>
+			</view>
+		</block>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "tabTop",
+		props: {
+			tabArr: Array,
+			default: () => []
+		},
+		data() {
+			return {
+				tabIndex: 0
+			};
+		},
+		methods: {
+			changeTabIndex(index) {
+				this.tabIndex = index
+				this.$emit('getTabIndex', this.tabIndex)
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- pages/bookDetail/bookDetail.vue
+++ pages/bookDetail/bookDetail.vue
@@ -4,37 +4,71 @@
 		<view style="background-color: #a8b0c3;height: 250rpx;" class="geryTheme flex align-center py-2">
 			<image src="../../static/Rebook/Rebook1.jpg" mode="widthFix" lazy-load class="flex-1 mx-2 rounded"></image>
 			<view class="flex-2 mx-2">
-				<view style="font-size: 45rpx;">西游记</view>
-				<view class="font mt-1">作者：吴承恩</view>
+				<view style="font-size: 45rpx;">{{name}}</view>
+				<view class="font mt-1">作者：{{author}}</view>
 				<view class="flex align-center mt-2">
 					<button type="default" class="flex-1 mx-2">分享</button>
 					<button type="default" class="flex-1 mx-2">收藏</button>
 				</view>
 			</view>
 		</view>
+
+		<view class="shadow" style="height: 1210rpx;">
+			<tab-top :tabArr="['详情','目录']" @getTabIndex='getTabIndex'></tab-top>
+
+			<!-- 详情 -->
+			<scroll-view scroll-y v-if="tabIndex === 0" style="height: 1129rpx;">
+				<view>
+					<view class="py-2 flex justify-center text-light-black font-lg">--简介--</view>
+					<view class="px-2 font-lg" style="line-height: 80rpx;">{{synopsis}}</view>
+				</view>
+			</scroll-view>
+
+			<!-- 目录 -->
+			<scroll-view scroll-y v-else style="height: 1129rpx;">
+				<block v-for="item in chapterCatalog" :key='item.id'>
+					<view class="p-2 text-ellipsis border-bottom" hover-class="bg-light" @tap="toReadingPage(item.id)">
+						{{item.title}}
+					</view>
+				</block>
+			</scroll-view>
+		</view>
 	</view>
 </template>
 
 <script>
 	import pageTitle from '@/components/pageTitle.vue'
+	import tabTop from '@/components/tabTop.vue'
+
+	import test from '@/common/test.js'
 
 	export default {
 		components: {
-			pageTitle
+			pageTitle,
+			tabTop
 		},
 		data() {
 			return {
-
+				name: test.name,
+				author: test.author,
+				synopsis: test.synopsis,
+				chapterCatalog: test.chapterCatalog,
+				tabIndex: 0
 			}
 		},
 		methods: {
-
+			getTabIndex(index) {
+				this.tabIndex = index
+			},
+			toReadingPage(id){
+				
+			}
 		}
 	}
 </script>
 
 <style>
-.geryTheme{
-	background-color: #a8b0c3;
-}
+	.geryTheme {
+		background-color: #a8b0c3;
+	}
 </style>
